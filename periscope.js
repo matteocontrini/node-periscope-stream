dd2e990eb79cab784862d540cbfe1bd6ef3fb6b2
Niemes <spookssi@gmail.com>
2017-10-22 01:35:18 +0200

    Update Url

--- periscope.js
+++ periscope.js
@@ -1,20 +1,41 @@
 var request = require('request');
 
 function periscope(url, callback) {
-	var id = url.match(/periscope.tv\/w\/(.*)/i)[1];
-	
-	var api = 'https://api.periscope.tv/api/v2/getAccessPublic?broadcast_id=' + id;
-	
-	request(api, function(error, response, body) {
-		if (error || response.statusCode != 200) {
-			callback('Response error: ' + response.statusCode);
-			return;
+
+
+	let idFinder = function(url) {
+
+		let pscp = url.match(/pscp.tv\/w\/(.*)/i);
+		let peri = url.match(/periscope.tv\/w\/(.*)/i);
+
+		if (pscp !== null) {
+			return pscp[1];
 		}
-		
-		var obj = JSON.parse(body);
-		
-		callback(null, obj);
-	});
+		if (peri !== null) {
+			return peri[1];
+		} else return null;
+
+	};
+
+	if (idFinder(url) !== null) {
+
+		var api = 'https://api.periscope.tv/api/v2/getAccessPublic?broadcast_id=' + idFinder(url);
+
+		request(api, function(error, response, body) {
+			if (error || response.statusCode != 200) {
+				callback('Response error: ' + response.statusCode);
+				return;
+			}
+
+			var obj = JSON.parse(body);
+
+			callback(null, obj);
+		});
+
+	} else {
+		return callback(null, "Not a valid URL.")
+	}
 }
 
 module.exports = periscope;
+
